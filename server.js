8a994b8e996b54222c2c62063ac66147e93f9743
dullyunderscorenew <mischrobin@gmail.com>
2026-06-10 21:26:12 +0200

    server logic implemented, db setup and style adjustments

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const { Pool } = require("pg");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running at http://localhost:" + PORT);
+});
